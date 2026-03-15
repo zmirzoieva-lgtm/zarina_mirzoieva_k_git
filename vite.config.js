3642765057ab2848fc8c23f0409ea77e0b50f6c2
Zarina Mirzoieva <z.mirzoieva@student.bachelor-goit.com.ua>
2026-03-15 13:16:27 +0200

    fix styles import

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(['./src//.html']),
       SortCss({
         sort: 'mobile-first',
       }),
